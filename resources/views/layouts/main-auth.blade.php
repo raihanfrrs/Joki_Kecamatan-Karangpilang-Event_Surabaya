5e6b2c2ea5f39554a36ef687e4d2607c69136840
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2023-06-29 10:11:11 +0700

    fixing vendor

--- resources/views/layouts/main-auth.blade.php
+++ resources/views/layouts/main-auth.blade.php
@@ -10,8 +10,6 @@
   <!-- ========== VENDOR CSS ========= -->
   <link rel="stylesheet" href="{{ asset('/') }}vendor/bootstrap-5.2.2-dist/css/bootstrap.min.css" />
   <link rel="stylesheet" href="{{ asset('/') }}vendor/bootstrap-icons/bootstrap-icons.css" />
-  <link rel="stylesheet" href="{{ asset('/') }}vendor/lineicons/lineicons.css" />
-  <link rel="stylesheet" href="{{ asset('/') }}vendor/materialdesignicons/materialdesignicons.min.css" />
   <link rel="stylesheet" href="{{ asset('/') }}vendor/datatables/css/datatables.min.css" />
   <link rel="stylesheet" href="{{ asset('/') }}vendor/sweetalert2/sweetalert2.min.css"/>
 
@@ -55,16 +53,10 @@
 
 
     <!-- Vendor JS Files -->
-    <script src="{{ asset('/') }}vendor/apexcharts/apexcharts.min.js"></script>
     <script src="{{ asset('/') }}vendor/jquery/jquery-3.6.1.min.js"></script>
     <script src="{{ asset('/') }}vendor/datatables/js/datatables.min.js"></script>
     <script src="{{ asset('/') }}vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('/') }}vendor/chart.js/chart.umd.js"></script>
-    <script src="{{ asset('/') }}vendor/echarts/echarts.min.js"></script>
-    <script src="{{ asset('/') }}vendor/quill/quill.min.js"></script>
     <script src="{{ asset('/') }}vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="{{ asset('/') }}vendor/tinymce/tinymce.min.js"></script>
-    <script src="{{ asset('/') }}vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
     <script src="{{ asset('/') }}js/datatables.min.js"></script>
